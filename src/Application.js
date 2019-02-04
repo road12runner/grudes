24191103e5c4e2179c08de1d2c1a0ae26b64a6be
dpanferov <dpanferov@inwk.com>
2019-02-04 10:07:20 +0100

    done

--- src/Application.js
+++ src/Application.js
@@ -3,27 +3,52 @@ import NewGrudge from './NewGrudge';
 import Grudges from './Grudges';
 import './Application.css';
 
+import Amplify from 'aws-amplify';
+import configuration from './aws-exports';
+import {withAuthenticator} from 'aws-amplify-react'
+import {API} from 'aws-amplify';
+
+Amplify.configure(configuration);
+
 class Application extends Component {
   state = {
     grudges: [],
   };
 
+  componentDidMount() {
+    API.get('grudgesCRUD', '/grudges').then( grudges => {
+      console.log('grudges', grudges);
+      this.setState({grudges});
+    }).catch( err => console.log('error', err));
+  }
+
   addGrudge = grudge => {
-    this.setState({ grudges: [grudge, ...this.state.grudges] });
+    API.post('grudgesCRUD', '/grudges', {body: grudge}).then( data => {
+      console.log('gredge added', data);
+      this.setState({ grudges: [grudge, ...this.state.grudges] });
+    });
+    
   };
 
   removeGrudge = grudge => {
-    this.setState({
-      grudges: this.state.grudges.filter(other => other.id !== grudge.id),
-    });
+    API.del('grudgesCRUD', '/grudges/object/' + grudge.id).then( () => {
+      this.setState({
+        grudges:  this.state.grudges.filter(other => other.id !== grudge.id),
+      });
+    })
   };
 
   toggle = grudge => {
-    const othergrudges = this.state.grudges.filter(
-      other => other.id !== grudge.id,
-    );
+
     const updatedGrudge = { ...grudge, avenged: !grudge.avenged };
-    this.setState({ grudges: [updatedGrudge, ...othergrudges] });
+    
+    API.put('grudgesCRUD', '/grudges', {body: updatedGrudge}).then( () => {
+      const othergrudges = this.state.grudges.filter(
+        other => other.id !== grudge.id,
+      );
+      
+      this.setState({ grudges: [updatedGrudge, ...othergrudges] });
+    })
   };
 
   render() {
@@ -51,4 +76,4 @@ class Application extends Component {
   }
 }
 
-export default Application;
+export default withAuthenticator(Application);
